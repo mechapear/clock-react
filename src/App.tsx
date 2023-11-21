2fff54eef2ebe97fb3211cc7f454324c8123c80a
mechapear <mechapear@gmail.com>
2023-11-21 16:05:52 +0700

    feat: add current date and time

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 
 export default function App() {
-  return <></>
+  const [date, setDate] = useState(new Date())
+
+  const day = date.toLocaleDateString(undefined, {
+    weekday: 'short',
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+  })
+
+  const time = date.toLocaleTimeString(undefined, {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
+  })
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    return () => clearInterval(interval)
+  })
+
+  return (
+    <>
+      <div>{day}</div>
+      <div>{time}</div>
+    </>
+  )
 }
