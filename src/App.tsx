fff1aaf87cd551bb70587f29346af483e5275558
mechapear <mechapear@gmail.com>
2023-11-21 16:50:10 +0700

    feat: add checkbox to toggle 24-hour format

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 
 export default function App() {
   const [date, setDate] = useState(new Date())
+  const [is24Hour, setIs24Hour] = useState(false)
 
   const day = date.toLocaleDateString(undefined, {
     weekday: 'short',
@@ -15,7 +16,7 @@ export default function App() {
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
-    hour12: false,
+    hour12: !is24Hour,
   })
 
   useEffect(() => {
@@ -23,12 +24,21 @@ export default function App() {
       setDate(new Date())
     }, 1000)
     return () => clearInterval(interval)
-  })
+  }, [])
 
   return (
     <>
       <div>{day}</div>
       <div>{time}</div>
+      <div>
+        <input
+          type="checkbox"
+          // set state from the latest value of is24Hour pasing function inside setIs24Hour
+          onChange={() => setIs24Hour((prevIs24Hour) => !prevIs24Hour)}
+          checked={is24Hour}
+        />
+        <label>24-hour time</label>
+      </div>
     </>
   )
 }
