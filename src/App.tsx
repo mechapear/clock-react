9d8b7b622a27d03c7cbc3bf955f9b9d9c88d4ae2
mechapear <mechapear@gmail.com>
2023-11-23 18:38:46 +0700

    feat: get and set user preference with local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,63 @@
 import { useEffect, useState } from 'react'
+import { MoonIcon, SunIcon } from './icons.tsx'
+
+// user preference information from local storage
+const userPreferenceStorage = {
+  time: {
+    key: 'time',
+    getIs24Hour: () => {
+      return localStorage.getItem(userPreferenceStorage.time.key) === '24'
+    },
+    setIs24Hour: (is24Hour: boolean) => {
+      localStorage.setItem(
+        userPreferenceStorage.time.key,
+        is24Hour ? '24' : '12',
+      )
+    },
+  },
+  theme: {
+    key: 'theme',
+    getIsDarkMode: () => {
+      return localStorage.getItem(userPreferenceStorage.theme.key) === 'dark'
+    },
+    setIsDarkMode: (isDarkMode: boolean) => {
+      localStorage.setItem(
+        userPreferenceStorage.theme.key,
+        isDarkMode ? 'dark' : 'light',
+      )
+    },
+  },
+}
+
+function initTheme() {
+  const isDarkMode = userPreferenceStorage.theme.getIsDarkMode()
+  if (isDarkMode) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+  return { isDarkMode }
+}
+
+function setDarkModeToRoot(isDark: boolean) {
+  if (isDark) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+  userPreferenceStorage.theme.setIsDarkMode(isDark)
+}
 
 export default function App() {
   const [date, setDate] = useState(new Date())
-  const [is24Hour, setIs24Hour] = useState(false)
-  const [isDarkMode, setIsDarkMode] = useState(false)
+  const [is24Hour, setIs24Hour] = useState(() => {
+    // check if the user has already set a clock format preference
+    return userPreferenceStorage.time.getIs24Hour()
+  })
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    // check if the user has already set a theme preference
+    return initTheme().isDarkMode
+  })
 
   const day = date.toLocaleDateString(undefined, {
     weekday: 'short',
@@ -26,32 +80,35 @@ export default function App() {
     return () => clearInterval(interval)
   }, [])
 
-  useEffect(() => {
-    // set the dark mode class to the root element
-    if (isDarkMode) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-  }, [isDarkMode]) // On page load or when changing themes
+  function handleChangeTheme() {
+    setIsDarkMode((prevIsDarkMode) => {
+      const nextIsDarkMode = !prevIsDarkMode
+      // set the dark mode class to the root element
+      setDarkModeToRoot(nextIsDarkMode)
+      return nextIsDarkMode
+    })
+  }
+
+  function handleChangeTimeFormat() {
+    setIs24Hour((prevIs24Hour) => {
+      // set state from the latest value of is24Hour pasing function inside setIs24Hour
+      const nextIs24Hour = !prevIs24Hour
+      // set the time format to the local storage
+      userPreferenceStorage.time.setIs24Hour(nextIs24Hour)
+      return nextIs24Hour
+    })
+  }
 
   return (
     <>
-      <nav className="sticky z-0 top-0 p-4 flex flex-row-reverse">
-        <label className="relative inline-flex items-center cursor-pointer">
-          <input
-            className="sr-only peer"
-            type="checkbox"
-            // set state from the latest value of isDarkMode pasing function inside setIsDarkMode
-            onChange={() => setIsDarkMode((prevIsDarkMode) => !prevIsDarkMode)}
-            checked={isDarkMode}
-          />
-          <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300  after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-          <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-            Dark mode
-          </span>
-        </label>
-      </nav>
+      <div className="fixed top-5 right-5 duration-100">
+        <button
+          className="w-8 h-8 m-1 bg-gray-200 dark:bg-gray-300 text-gray-900 dark:text-gray-700 dark:border-gray-400 grid place-content-center rounded-full ring-1 ring-inset ring-black/10 shadow"
+          onClick={handleChangeTheme}
+        >
+          {isDarkMode ? <SunIcon /> : <MoonIcon />}
+        </button>
+      </div>
 
       <div className="text-center absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 ">
         <div className="text-sm font-medium text-gray-900 dark:text-gray-300">
@@ -65,8 +122,7 @@ export default function App() {
             <input
               className="sr-only peer"
               type="checkbox"
-              // set state from the latest value of is24Hour pasing function inside setIs24Hour
-              onChange={() => setIs24Hour((prevIs24Hour) => !prevIs24Hour)}
+              onChange={handleChangeTimeFormat}
               checked={is24Hour}
             />
             <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
